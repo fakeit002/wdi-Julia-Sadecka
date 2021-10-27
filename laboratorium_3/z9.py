476c88dfad5df742585de293e919dac8ea763aa7
fakeit002 <sadecka@student.agh.edu.pl>
2021-10-27 17:40:56 +0200

    changes in z9

--- laboratorium_3/z9.py
+++ laboratorium_3/z9.py
@@ -1,17 +1,28 @@
-saldo = 250000000000
-a = ("Wpłata")
-b = ("Wypłata")
-c = ("Sprawdź saldo" or "sprawdź saldo")
-x = int(input("Wpisz PIN"))
+saldo = 2500
+a = ("1")
+b = ("2")
+c = ("3")
+x = int(input("Wprowadź PIN"))
 if x != 1234:
     print("ZŁY PIN, MASZ JESZCZE JEDNĄ SZANSĘ, ALBO ŚRODKI Z TWOJEGO KONTA ZOSTANĄ SKASOWANE" )
 elif x == 1234:
-    d = input("Co chcesz zrobić? Wpłata Wypłata Sprawdź saldo ")
+    d = input("Co chcesz zrobić? Wpłata-wprowadź '1'; Wypłata-wprowadź '2'; Sprawdź saldo-wprowadź '3' ")
     if d == c:
+        print("Stan twoje konta to:")
         print(saldo)
     if d == a:
         e = int(input("Ile?"))
-        print(saldo + e)
+        print("Stan twojego konta to: ")
+        saldo = saldo + e
+        print(saldo+e)
+    if d == b:
+        f = int(input("Ile?"))
+        if f>saldo:
+            print("Kwota nie może być większa niż stan konta")
+            y = input("Czy chcesz wypłacić mniejszą kwotę? [tak/nie]")
+            if y == tak:
+
+
 
 
 
